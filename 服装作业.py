bebe97b21c972e04c13852ca6e99777c7563b6a6
“liu972922894” <972922894@qq.com>
2021-04-21 17:10:34 +0800

    服装作业.py

---
+++ 服装作业.py
@@ -0,0 +1,285 @@
+# 第1件衣服
+riqi1 = "1号"
+fuzhang1 = "羽绒服"
+jiage1 = 253.6
+kucun1 = 500
+xiaoliang1 = 10
+
+# 第2件衣服
+riqi2 = "2号"
+fuzhang2 = "牛仔裤"
+jiage2 = 86.3
+kucun2 = 600
+xiaoliang2 = 60
+
+# 第3件衣服
+riqi3 = "3号"
+fuzhang3 = "风衣"
+jiage3 = 96.8
+kucun3 = 335
+xiaoliang3 = 43
+
+# 第4件衣服
+riqi4 = "4号"
+fuzhang4 = "皮草"
+jiage4 = 135.9
+kucun4 = 855
+xiaoliang4 = 63
+
+# 第5件衣服
+riqi5 = "5号"
+fuzhang5 = "T桖"
+jiage5 = 65.8
+kucun5 = 632
+xiaoliang5 = 63
+
+# 第6件衣服
+riqi6 = "6号"
+fuzhang6 = "衬衫"
+jiage6 = 49.3
+kucun6 = 562
+xiaoliang6 = 120
+
+# 第7件衣服
+riqi7 = "7号"
+fuzhang7 = "牛仔裤"
+jiage7 = 86.3
+kucun7 = 600
+xiaoliang7 = 72
+
+# 第8件衣服
+riqi8 = "8号"
+fuzhang8 = "羽绒服"
+jiage8 = 253.6
+kucun8 = 500
+xiaoliang8 = 69
+
+# 第9件衣服
+riqi9 = "9号"
+fuzhang9 = "牛仔裤"
+jiage9 = 86.3
+kucun9 = 335
+xiaoliang9 = 35
+
+# 第10件衣服
+riqi10 = "10号"
+fuzhang10 = "羽绒服"
+jiage10 = 253.6
+kucun10 = 500
+xiaoliang10 = 140
+
+# 第11件衣服
+riqi11 = "11号"
+fuzhang11 = "牛仔库"
+jiage11 = 86.3
+kucun11 = 600
+xiaoliang11 = 90
+
+# 第12件衣服
+riqi12 = "12号"
+fuzhang12 = "皮草"
+jiage12 = 135.9
+kucun12 = 855
+xiaoliang12 = 24
+
+# 第13件衣服
+riqi13 = "13号"
+fuzhang13 = "T桖"
+jiage13 = 65.8
+kucun13 = 632
+xiaoliang13 = 45
+
+# 第14件衣服
+riqi14 = "14号"
+fuzhang14 = "风衣"
+jiage14 = 96.8
+kucun14 = 335
+xiaoliang14 = 25
+
+# 第15件衣服
+riqi15 = "15号"
+fuzhang15 = "牛仔裤"
+jiage15 = 86.3
+kucun15 = 600
+xiaoliang15 = 60
+
+# 第16件衣服
+riqi16 = "16号"
+fuzhang16 = "T桖"
+jiage16 = 65.8
+kucun16 = 632
+xiaoliang16 = 129
+
+# 第17件衣服
+riqi17 = "17号"
+fuzhang17 = "羽绒服"
+jiage17 = 253.6
+kucun17 = 500
+xiaoliang17 = 10
+
+# 第18件衣服
+riqi18 = "18号"
+fuzhang18 = "风衣"
+jiage18 = 96.8
+kucun18 = 335
+xiaoliang18 = 43
+
+# 第19件衣服
+riqi19 = "19号"
+fuzhang19 = "T桖"
+jiage19 = 65.8
+kucun19 = 632
+xiaoliang19 = 63
+
+# 第20件衣服
+riqi20 = "20号"
+fuzhang20 = "羽绒服"
+jiage20 = 253.6
+kucun20 = 500
+xiaoliang20 = 10
+
+# 第21件衣服
+riqi21 = "21号"
+fuzhang21 = "皮草"
+jiage21 = 135.9
+kucun21 = 855
+xiaoliang21 = 63
+
+# 第22件衣服
+riqi22 = "22号"
+fuzhang22 = "风衣"
+jiage22 = 96.8
+kucun22 = 335
+xiaoliang22 = 60
+
+# 第23件衣服
+riqi23 = "23号"
+fuzhang23 = "T桖"
+jiage23 = 65.8
+kucun23 = 632
+xiaoliang23 = 58
+
+# 第24件衣服
+riqi24 = "24号"
+fuzhang24 = "牛仔裤"
+jiage24 = 86.3
+kucun24 = 600
+xiaoliang24 = 140
+
+# 第25件衣服
+riqi25 = "25号"
+fuzhang25 = "T桖"
+jiage25 = 65.8
+kucun25 = 632
+xiaoliang25 = 48
+
+# 第26件衣服
+riqi26 = "26号"
+fuzhang26 = "风衣"
+jiage26 = 96.8
+kucun26 = 335
+xiaoliang26 = 43
+
+# 第27件衣服
+riqi27 = "27号"
+fuzhang27 = "皮草"
+jiage27 = 135.9
+kucun27 = 855
+xiaoliang27 = 57
+
+# 第28件衣服
+riqi28 = "28号"
+fuzhang28 = "羽绒服"
+jiage28 = 253.6
+kucun28 = 500
+xiaoliang28 = 10
+
+# 第29件衣服
+riqi29 = "23号"
+fuzhang29 = "T桖"
+jiage29 = 65.8
+kucun29 = 632
+xiaoliang29 = 63
+
+# 第30件衣服
+riqi30 = "30号"
+fuzhang30 = "风衣"
+jiage30 = 96.8
+kucun30 = 335
+xiaoliang30 = 78
+
+print("------------服装商城--------------")
+print("日期\t\t服装名称\t\t价格\t\t库存数量\t\t销售量")
+print(riqi1, "\t\t", fuzhang1, "\t\t", jiage1, "\t\t", kucun1, "\t\t", xiaoliang1)
+print(riqi2, "\t\t", fuzhang2, "\t\t", jiage2, "\t\t", kucun2, "\t\t", xiaoliang2)
+print(riqi3, "\t\t", fuzhang3, "\t\t", jiage3, "\t\t", kucun3, "\t\t", xiaoliang3)
+print(riqi4, "\t\t", fuzhang4, "\t\t", jiage4, "\t\t", kucun4, "\t\t", xiaoliang4)
+print(riqi5, "\t\t", fuzhang5, "\t\t", jiage5, "\t\t", kucun5, "\t\t", xiaoliang5)
+print(riqi6, "\t\t", fuzhang6, "\t\t", jiage6, "\t\t", kucun6, "\t\t", xiaoliang6)
+print(riqi7, "\t\t", fuzhang7, "\t\t", jiage7, "\t\t", kucun7, "\t\t", xiaoliang7)
+print(riqi8, "\t\t", fuzhang8, "\t\t", jiage8, "\t\t", kucun8, "\t\t", xiaoliang8)
+print(riqi9, "\t\t", fuzhang9, "\t\t", jiage9, "\t\t", kucun9, "\t\t", xiaoliang9)
+print(riqi10, "\t\t", fuzhang10, "\t\t", jiage10, "\t\t", kucun10, "\t\t", xiaoliang10)
+print(riqi11, "\t\t", fuzhang11, "\t\t", jiage11, "\t\t", kucun11, "\t\t", xiaoliang11)
+print(riqi12, "\t\t", fuzhang12, "\t\t", jiage12, "\t\t", kucun12, "\t\t", xiaoliang12)
+print(riqi13, "\t\t", fuzhang13, "\t\t", jiage13, "\t\t", kucun13, "\t\t", xiaoliang13)
+print(riqi14, "\t\t", fuzhang14, "\t\t", jiage14, "\t\t", kucun14, "\t\t", xiaoliang14)
+print(riqi15, "\t\t", fuzhang15, "\t\t", jiage15, "\t\t", kucun15, "\t\t", xiaoliang15)
+print(riqi16, "\t\t", fuzhang16, "\t\t", jiage16, "\t\t", kucun16, "\t\t", xiaoliang16)
+print(riqi17, "\t\t", fuzhang17, "\t\t", jiage17, "\t\t", kucun17, "\t\t", xiaoliang17)
+print(riqi18, "\t\t", fuzhang18, "\t\t", jiage18, "\t\t", kucun18, "\t\t", xiaoliang18)
+print(riqi19, "\t\t", fuzhang19, "\t\t", jiage19, "\t\t", kucun19, "\t\t", xiaoliang19)
+print(riqi20, "\t\t", fuzhang20, "\t\t", jiage20, "\t\t", kucun20, "\t\t", xiaoliang20)
+print(riqi21, "\t\t", fuzhang21, "\t\t", jiage21, "\t\t", kucun21, "\t\t", xiaoliang21)
+print(riqi22, "\t\t", fuzhang22, "\t\t", jiage22, "\t\t", kucun22, "\t\t", xiaoliang22)
+print(riqi23, "\t\t", fuzhang23, "\t\t", jiage23, "\t\t", kucun23, "\t\t", xiaoliang23)
+print(riqi24, "\t\t", fuzhang24, "\t\t", jiage24, "\t\t", kucun24, "\t\t", xiaoliang24)
+print(riqi25, "\t\t", fuzhang25, "\t\t", jiage25, "\t\t", kucun25, "\t\t", xiaoliang25)
+print(riqi26, "\t\t", fuzhang26, "\t\t", jiage26, "\t\t", kucun26, "\t\t", xiaoliang26)
+print(riqi27, "\t\t", fuzhang27, "\t\t", jiage27, "\t\t", kucun27, "\t\t", xiaoliang27)
+print(riqi28, "\t\t", fuzhang28, "\t\t", jiage28, "\t\t", kucun28, "\t\t", xiaoliang28)
+print(riqi29, "\t\t", fuzhang29, "\t\t", jiage29, "\t\t", kucun29, "\t\t", xiaoliang29)
+print(riqi30, "\t\t", fuzhang30, "\t\t", jiage30, "\t\t", kucun30, "\t\t", xiaoliang30)
+
+# 总销量
+a = round((jiage1*xiaoliang1+jiage2*xiaoliang2+jiage3*xiaoliang3+jiage4*xiaoliang4+jiage5*xiaoliang5+jiage6*xiaoliang6+jiage7*xiaoliang7+jiage8*xiaoliang8+jiage9*xiaoliang9+jiage10*xiaoliang10+jiage11*xiaoliang11+jiage12*xiaoliang12+jiage13*xiaoliang13+jiage14*xiaoliang14+jiage15*xiaoliang15+jiage16*xiaoliang16+jiage17*xiaoliang17+jiage18*xiaoliang18+jiage19*xiaoliang19+jiage20*xiaoliang20+jiage21*xiaoliang21+jiage22*xiaoliang22+jiage23*xiaoliang23+jiage24*xiaoliang24+jiage25*xiaoliang25+jiage26*xiaoliang26+jiage27*xiaoliang27+jiage28*xiaoliang28+jiage29*xiaoliang29+jiage30*xiaoliang30),  2)
+
+# 羽绒服所有的销售量
+b = round((jiage1*xiaoliang1+jiage8*xiaoliang8+jiage17*xiaoliang17+jiage28*xiaoliang28),2)
+
+# 羽绒服占比
+print("销售额占比", b / a)
+
+# 羽绒服一件衣服的价格
+c = (jiage1)
+
+# 羽绒服所有的销售量
+d = round((jiage1*xiaoliang1+jiage8*xiaoliang8+jiage17*xiaoliang17+jiage28*xiaoliang28),2)
+
+# 每件衣服的占比
+print("每件衣服的占比", c / d)
+
+#print("输入金额 : $", a )
+#print("羽绒服占比 ：$", (jiage1*xiaoliang1+jiage8*xiaoliang8+jiage17*xiaoliang17+jiage28*xiaoliang28) /)
+#print("羽绒服销售额占比 ： $", (jiage1*xiaoliang1+jiage8*xiaoliang8+jiage17*xiaoliang17+jiage28*xiaoliang28) / 195758)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
